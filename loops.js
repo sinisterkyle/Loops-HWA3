3243b4ce24075f8f6571e2629e130ff0ea4297a7
ALiberatoreV <aliberatore@mail.valenciacollege.edu>
2020-09-22 16:19:31 -0400

    initial commit

---
+++ loops.js
@@ -0,0 +1,13 @@
+for(let i = 1000; i >= 0; i -= 2) {
+    console.log(i)
+}
+for(let count = 0; count <= 10000; count++);
+
+
+
+const tvShows = ["My #1 favorite tv show is Vikings", "My #2 favorite tv show is Stranger Things", 
+"My #3 favorite tv show is The Wicher", "My #4 favorite tv show is X Files", "My #5 favorite tv show is Archer"];
+
+for(let i = 0; i < tvShows.length; i++) {
+        console.log(tvShows[i])
+} 
